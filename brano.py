857967b01fbf30ac58e086971bff4dbcb87acfe7
LorenzoCollivaUnibo <lorenzo.colliva03@gmail.com>
2025-11-19 18:19:58 +0100

    Fix comments for clarity in brano.py

--- brano.py
+++ brano.py
@@ -7,7 +7,7 @@ audio_path = "MUEVELOU.mp3"
 #1. Upload music
 y, sr = librosa.load(audio_path)
 
-#2. Find BPM (average tempo) and beats
+#2. Find BPM (average time) and beats
 tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
 tempo_val = float(tempo)
 print(f"BPM medio: {tempo_val:.1f}")
@@ -16,7 +16,7 @@ beat_times = librosa.frames_to_time(beats, sr=sr)
 
 #3. Calculate RMS power (sound intensity level)
 rms = librosa.feature.rms(y=y)[0]
-rms = rms / max(rms)  # normalizza tra 0 e 1
+rms = rms / max(rms)  #normalize between 0 and 1
 frames = range(len(rms))
 times = librosa.frames_to_time(frames, sr=sr)
 
